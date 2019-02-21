f2778b8e63de05ff0d97f6135d5141f9ddc028a3
kfchou <kfchou@bu.edu>
2019-02-21 16:45:02 -0500

    Update regroup

--- mechs/regroup.m
+++ mechs/regroup.m
@@ -18,3 +18,9 @@ for i = 1:group2
     idxOut = 1+(group1)*(i-1):group1*i;
     out(idxOut,:) = temp(idxIn,:);
 end
+% prefFreq = repmat((1:36),1,5)';
+% prefLocs = [ones(1,36) ones(1,36)*2 ones(1,36)*3 ones(1,36)*4 ones(1,36)*5]';
+% figure;
+% subplot(131); imagesc(in); title('initial connectivity matrix')
+% subplot(132); imagesc(temp); title('intermediate')
+% subplot(133); imagesc(out); title('regrouped connectivity matrix')
