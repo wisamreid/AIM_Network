b50d234342821000a99dd5c23309fe1f0c574222
kfchou <kfchou@bu.edu>
2019-02-20 22:04:24 -0500

    Update 2d implementation

--- mechs/genICinput.m
+++ mechs/genICinput.m
@@ -4,6 +4,10 @@ function sigIn = genICinput(tauR, tauD, dt)
 %   dt = sampling frequency of IC data
 % output:
 %   sigIn = a matrix of EPSP waveforms, with dimensions time x (nfreqs x nlocs)
+%
+% @ Erik Roberts, Kenny Chou
+% Boston Univeristy, 2019
+%
 % TODO: allow resampling of spk_IC to different dt
 
 try
@@ -25,7 +29,8 @@ nlocs = size(spk_IC,3);
 % Reshape data into a 2D matrix that is (time) x (frequencies x locations)
 sigIn = reshape(spk_IC,timeLen,nfreqs*nlocs);
 
-%% create EPSP waveform - convolve each time series with epsc waveform
+% ========================= create EPSP waveform =========================
+% ============= convolve each time series with epsc waveform ==============
 t_a = max(tauR,tauD)*7; % Max duration of syn conductance
 t_vec = 0:dt:t_a;
 tau2 = tauR;
@@ -33,7 +38,6 @@ tau1 = tauD;
 tau_rise = tau1*tau2/(tau1-tau2);
 b = ((tau2/tau1)^(tau_rise/tau1) - (tau2/tau1)^(tau_rise/tau2)^-1); % /tau2?
 epsc =  - b * ( exp(-t_vec/tau1) - exp(-t_vec/tau2) ); % - to make positive
-
 sigIn = conv2(sigIn,epsc,'same');
 
 end
