52fe146ad1bc5e752a11d1be15e562585d9526af
kfchou <kfchou@bu.edu>
2019-09-23 17:32:30 -0400

    add comments

--- movxcorrKC.m
+++ movxcorrKC.m
@@ -1,5 +1,6 @@
 function cc = movxcorrKC(a,b,framelen)
-
+% compute normalized cross-correlation between a and b by taking a windowed
+% frame with length framelen for 
 i = 1;
 while i+framelen < size(a,2)
     smalla = a(:,i:i+framelen-1);
